a8b2932123b453f651494f043d2e5d91ec56e345
Mhmd-S <65135074+Mhmd-S@users.noreply.github.com>
2022-11-18 19:58:27 +0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Form from './Components/Form';
 
 function App() {
+  const [rolodex, setRolodex] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Form  hoistDataForm={(data)=>{setRolodex([...rolodex, data])}}/>
     </div>
   );
 }
---
+++ src/Components/Form.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useForm } from "react-hook-form"
+
+const Form = ({hoistDataForm}) => {
+
+    const {register, handleSubmit} = useForm();
+
+    return(
+        <form onSubmit={handleSubmit((data)=>{hoistDataForm(data)})}>
+            <label>First Name</label>
+            <input {...register("firstName")} type="text"/>
+            <label>Middle Name</label>
+            <input {...register("middleName")} type="text"/>
+            <label>Last Name</label>
+            <input {...register("lastName")} type="text"/>
+            <label>Date of Birth</label>
+            <input {...register("dob")} type="date"/>
+            <label>Field/Occupation</label>
+            <input {...register("occupation")} type="text"/>
+            <label>Contact Information</label>
+            <input {...register("contactInfo")} type="text"/>
+            <label>Notes</label>
+            <input {...register("notes")} type="text"/>
+            <button type='submit'>Submit</button>
+        </form>
+    )
+}
+
+export default Form;
